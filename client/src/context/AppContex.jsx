57d38cde90ea0f39135e68fe16ee921d9391cfd5
rexxy0011 <ifeanyiumeh47@gmail.com>
2026-03-10 21:52:07 +0100

    added more components to the pages folder

---
+++ client/src/context/AppContex.jsx
@@ -0,0 +1,14 @@
+import {createContext} from "react";
+
+
+export const AppContext = createdContext()
+
+
+export const AppContextProvider = (props)=> {
+  return (
+    <AppContext.Provider>
+      {}
+
+    </AppContext.Provider>
+  )
+}
