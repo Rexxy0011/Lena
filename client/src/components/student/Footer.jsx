688f951dcdf0f2610a9ab15a5fd0909db0d79f17
rexxy0011 <ifeanyiumeh47@gmail.com>
2026-03-20 07:47:52 +0100

    mounted footer

--- client/src/components/student/Footer.jsx
+++ client/src/components/student/Footer.jsx
@@ -1,11 +1,41 @@
-import React from "react"
+import React from "react";
+import { assets } from "../../assets/assets";
 
 const Footer = () => {
   return (
-    <div>
-      <h1>Footer</h1>
-
-    </div>
-  )
-}
-export default Footer
+    <footer className="bg-gray-900 md:px-36 text-left w-full mt-10">
+      <div className="flex flex-col md:flex-row items-start px-8 md:px-0 justify-center gap-10 md:gap-32 py-10 border-b border-white/30">
+        <div className="flex flex-col md:items-start items-center w-full">
+          <img src={assets.logo_dark} alt="" />
+          <p className="mt-6 text-center md:text-left text-sm text-white/80">
+            A modern learning platform built to help students and teams learn
+            faster with expert-led courses, structured paths, and progress you
+            can track.
+          </p>
+        </div>
+        <div className="flex flex-col md:items-start items-center w-full">
+          <h2 className="font-semibold text-white mb-5">Company</h2>
+          <ul className="flex md:flex-col w-full justify-between text-sm text-white/80 md:space-y-2">
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">About us</a>
+            </li>
+            <li>
+              <a href="#">Contact us</a>
+            </li>
+            <li>
+              <a href="#">Privacy policy</a>
+            </li>
+          </ul>
+        </div>
+        <div></div>
+      </div>
+      <p className="py-4 text-center text-xs md:text-sm text-white/60">
+        Copyright 2026 © Lena. All rights reserved.
+      </p>
+    </footer>
+  );
+};
+export default Footer;
