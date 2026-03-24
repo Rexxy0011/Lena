09b9379648d548cd3f72fb97027bee34d04832d5
rexxy0011 <ifeanyiumeh47@gmail.com>
2026-03-24 08:46:54 +0100

    course details

--- client/src/pages/student/CourseDetails.jsx
+++ client/src/pages/student/CourseDetails.jsx
@@ -1,11 +1,31 @@
-import React from "react"
+import React, { useContext, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { AppContext } from "../../context/AppContex";
 
 const CourseDetails = () => {
-  return (
-    <div>
-      <h1>CourseDetails</h1>
-    </div>
-  )
-}
+  const { id } = useParams();
+  const [courseData, setCourseData] = useState(null);
 
-export default CourseDetails
+  const [allCourses] = useContext(AppContext);
+
+  const fetchCourseData = async () => {
+    const findCourse = allCourses.find((course) => course._id === id);
+    setCourseData(findCourse);
+  };
+
+  useEffect(() => {
+    fetchCourseData();
+  });
+
+  return (
+    <div className="flex md:flex-row flex-col-reverse gap-10 relative items-start justify-between md:px-36 px-8 md:pt30 pt-20 text-left">
+      <div className="absolute top-0 left-0 w-full h-section-height -z-1 bg-gradient- to- b from-cyan-100/70"></div>
+      {/* left column */}
+      <div></div>
+      {/* Right column  */}
+      <div></div>
+    </div>
+  );
+};
+
+export default CourseDetails;
