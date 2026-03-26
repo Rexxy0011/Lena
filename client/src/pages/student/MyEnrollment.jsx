830d8fc01cf059ca1198be81f5f746880cf4a5c0
rexxy0011 <ifeanyiumeh47@gmail.com>
2026-03-26 15:03:39 +0100

    my enrollment

--- client/src/pages/student/MyEnrollment.jsx
+++ client/src/pages/student/MyEnrollment.jsx
@@ -1,11 +1,21 @@
-import React from "react"
+import React from "react";
 
 const MyEnrollment = () => {
   return (
-    <div>
-      <h1>MyEnrollment</h1>
+    <div className="md:px-36 px-8 pt-10">
+      <h1 className="text-2xl font-semibold">MyEnrollment</h1>
+      <table className="md:table-auto table-fixed w-full overflow-hidden border mt-10">
+        <thead>
+          <tr>
+            <th className="px-4 py-3 font-semibold truncate">Course</th>
+            <th className="px-4 py-3 font-semibold truncate">Duration</th>
+            <th className="px-4 py-3 font-semibold truncate">Completed</th>
+            <th className="px-4 py-3 font-semibold truncate">Status</th>
+          </tr>
+        </thead>
+      </table>
     </div>
-  )
-}
+  );
+};
 
-export default MyEnrollment
+export default MyEnrollment;
